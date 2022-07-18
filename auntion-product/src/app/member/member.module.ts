6473484a3b902d785f3fc19b61b841867ea4b093
A0721I1 - Le Tran Son <letranson1992@gmail.com>
2022-07-18 19:20:12 +0700

    Update code profile member

--- auntion-product/src/app/member/member.module.ts
+++ auntion-product/src/app/member/member.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { MemberRoutingModule } from './member-routing.module';
 import { ListMemberComponent } from './list-member/list-member.component';
 import { SignUpMemberComponent } from './sign-up-member/sign-up-member.component';
+import { ProfileMemberComponent } from './profile-member/profile-member.component';
+import { EditMemberComponent } from './edit-member/edit-member.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [ListMemberComponent, SignUpMemberComponent],
+  declarations: [ListMemberComponent, SignUpMemberComponent, ProfileMemberComponent, EditMemberComponent],
   imports: [
     CommonModule,
-    MemberRoutingModule
+    MemberRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class MemberModule { }
--- auntion-product/src/app/member/service/member.service.ts
+++ auntion-product/src/app/member/service/member.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Member} from '../../model/Member';
+
+
+const URL_API = 'http://localhost:8080/member/profile/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
-  constructor() { }
+  message: string;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  // SonLT View-Member
+  findByIdAccount(idAccount: number): Observable<Member>{
+    return this.httpClient.get<Member>(URL_API + 10);
+  }
+
+  // SonLT Edit-Member
+  editMember(member: Member): Observable<void> {
+    // @ts-ignore
+    return this.httpClient.put(URL_API + '/edit/' + 10 , member);
+  }
+
 }
