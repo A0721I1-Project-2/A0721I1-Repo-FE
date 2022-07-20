7ee23b28d8193834749fed9736436a6286227bd3
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-07-20 11:09:06 +0700

    fix css

--- auntion-product/src/app/home/service/home.service.ts
+++ auntion-product/src/app/home/service/home.service.ts
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Product} from '../../model/Product';
 import {Question} from '../../model/Question';
 import { Topic } from 'src/app/model/Topic';
-
-const URL_HOME_API = 'http://localhost:8080/manager/product/api';
-const URL_INTRUCTION_API = 'http://localhost:8080/manager/api/question';
-=======
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Product} from '../../model/Product';
-
 const URL_HOME_API = 'http://localhost:8080/manager/product/api';
+const URL_INTRUCTION_API = 'http://localhost:8080/manager/api/question';
 const URL_ACCOUNT_API = 'http://localhost:8080/manager/account/api';
->>>>>>> 087cfd249453a90801e18fb2821673eeaeed8176
-
 @Injectable({
   providedIn: 'root'
 })
@@ -26,8 +15,6 @@ export class HomeService {
   constructor(private httpClient: HttpClient) {
   }
 
-<<<<<<< HEAD
-=======
   // VinhTQ
   processForgotPasswordForm(username: string, email: string): Observable<string> {
     return this.httpClient.get<string>(URL_ACCOUNT_API + '/forgot-password?email=' + email + '&username=' + username);
@@ -43,7 +30,6 @@ export class HomeService {
     return this.httpClient.get<Product>(URL_HOME_API + '/find-by-id/' + id);
   }
 
->>>>>>> 087cfd249453a90801e18fb2821673eeaeed8176
   // HauLST
   showListProductAuction(): Observable<Product[]> {
     return this.httpClient.get<Product[]>(URL_HOME_API + '/list/auction');
@@ -62,11 +48,7 @@ export class HomeService {
   // HauLST
   searchListProduct(nameProduct: string, typeProductname: string, min: number, max: number) {
     // tslint:disable-next-line:max-line-length
-<<<<<<< HEAD
     return this.httpClient.get<Product[]>(URL_HOME_API + '/list/search' + '/name=' + nameProduct + '/type-product=' + typeProductname + '/' + min + '/' + max);
-=======
-    return this.httpClient.get<Product[]>(URL_HOME_API + '/list/search/' + '/name=' + nameProduct + '/type-product=' + typeProductname + '/' + min + '/' + max);
->>>>>>> 087cfd249453a90801e18fb2821673eeaeed8176
   }
 
   // HauLST
@@ -74,7 +56,6 @@ export class HomeService {
     // tslint:disable-next-line:max-line-length
     return this.httpClient.get<Product[]>(URL_HOME_API + '/list/search' + '/name=' + nameProduct + '/type-product=' + typeProductname + '/' + min);
   }
-<<<<<<< HEAD
 
   showListQuestion(): Observable<Question[]> {
     return this.httpClient.get<Question[]>(URL_INTRUCTION_API + '/list');
@@ -83,6 +64,4 @@ export class HomeService {
   listTopic(): Observable<Topic[]> {
     return this.httpClient.get<Topic[]>(URL_INTRUCTION_API + '/topic');
   }
-=======
->>>>>>> 087cfd249453a90801e18fb2821673eeaeed8176
 }
