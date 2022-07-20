614359838b10789a55d39d1e1fb320872d4fe239
Hieu Dinh Van <dvhieu.espd.ufl@gmail.com>
2022-07-20 18:03:18 +0700

    HieuDV update list product

--- auntion-product/src/app/product/product.module.ts
+++ auntion-product/src/app/product/product.module.ts
@@ -6,13 +6,15 @@ import { StatisticComponent } from './statistic/statistic.component';
 import { ListProductComponent } from './list-product/list-product.component';
 import { ReviewProductComponent } from './review-product/review-product.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [StatisticComponent, ListProductComponent, ReviewProductComponent, EditProductComponent],
-  imports: [
-    CommonModule,
-    ProductRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ProductRoutingModule,
+        ReactiveFormsModule
+    ]
 })
 export class ProductModule { }
--- auntion-product/src/app/product/service/product.service.ts
+++ auntion-product/src/app/product/service/product.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from '../../model/Product';
+const API_URL = 'http://localhost:8080/manager/product/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(page: number): Observable<Product[]> {
+    return this.http.get<Product[]>(API_URL + '/list?page=' + page );
+  }
+
+  getAllNotPagination(): Observable<Product[]> {
+    return this.http.get<Product[]>(API_URL + '/list-not-pagination');
+  }
+
+  findByID(id: number): Observable<Product> {
+    return this.http.get<Product>(`${API_URL}/product-detail?id=` + id);
+  }
+
+  searchByNameTypeSellerPriceStatus(name: string, typeProduct: number,
+                                    sellerName: string, maxPrice: number,
+                                    minPrice: number, biddingStatus: number,
+                                    page: number): Observable<Product[]> {
+    return this.http.get<Product[]>(`${API_URL}/search?name=` + name + '&sellerName=' + sellerName + '&maxPrice=' + maxPrice +
+      '&minPrice=' + minPrice + '&biddingStatus=' + biddingStatus + '&page=' + page);
+  }
+
+  searchByNameTypeSellerPriceStatusNotPagination(name: string, typeProduct: number,
+                                                 sellerName: string, maxPrice: number,
+                                                 minPrice: number, biddingStatus: number,
+                                                 page: number): Observable<Product[]> {
+    return this.http.get<Product[]>(`${API_URL}/search-not-pagination?name=` + name + '&sellerName=' + sellerName + '&maxPrice=' + maxPrice +
+      '&minPrice=' + minPrice + '&biddingStatus=' + biddingStatus);
+  }
+
+  updateProductStatus(product): Observable<Product> {
+    return this.http.post<Product>(API_URL + '/update-bidding-status', product);
+  }
 }
