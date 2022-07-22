c723150c3b61ee37fab6349f226cae5d0a03129f
phanthanhtoan <toan27122404@gmail.com>
2022-07-22 15:17:28 +0700

    edit-product

--- auntion-product/src/app/product/product.module.ts
+++ auntion-product/src/app/product/product.module.ts
@@ -6,13 +6,18 @@ import { StatisticComponent } from './statistic/statistic.component';
 import { ListProductComponent } from './list-product/list-product.component';
 import { ReviewProductComponent } from './review-product/review-product.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from './environments/environment';
 
 
 @NgModule({
   declarations: [StatisticComponent, ListProductComponent, ReviewProductComponent, EditProductComponent],
-  imports: [
-    CommonModule,
-    ProductRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ProductRoutingModule,
+        ReactiveFormsModule,
+        AngularFireModule.initializeApp(environment.firebase)
+    ]
 })
 export class ProductModule { }
--- auntion-product/src/app/product/service/product.service.ts
+++ auntion-product/src/app/product/service/product.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Product} from '../../model/Product';
+import {Observable} from 'rxjs';
+import {TypeProduct} from '../../model/TypeProduct';
+import {ImageProduct} from '../../model/ImageProduct';
+import {Member} from "../../model/Member";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  URL_PRODUCT = 'http://localhost:8080/manager/product';
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-  constructor() { }
+
+  findById(id: any): Observable<Product>{
+    return this.http.get<Product>(this.URL_PRODUCT + '/get/' + id );
+  }
+
+  findType(): Observable<TypeProduct[]> {
+    return this.http.get<TypeProduct[]>(this.URL_PRODUCT + '/type');
+  }
+
+  findImage(): Observable<ImageProduct[]> {
+    return this.http.get<ImageProduct[]>(this.URL_PRODUCT + '/img');
+  }
+
+  updateProduct(product: Product): Observable<void> {
+    return this.http.patch<void>(this.URL_PRODUCT + '/edit', product);
+  }
+  findByIdMember(id: any): Observable<Member>{
+    return this.http.get<Member>(this.URL_PRODUCT + '/member/' + id );
+  }
 }
