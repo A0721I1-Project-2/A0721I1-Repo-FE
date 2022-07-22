5a4f314c32c272f227ecf12a14dbe569579da15d
Bach2k2 <letrongbach02@gmail.com>
2022-07-22 16:13:39 +0700

    BachLT-statistic

--- auntion-product/src/app/admin/dashboard/dashboard.component.ts
+++ auntion-product/src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,57 @@
 import {Component, OnInit} from '@angular/core';
+import {Chart} from 'chart.js';
+import {
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
+import {registerables} from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,18 +60,47 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  userVisitChart: any;
+  defLabelArr: any[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  defDataArr: any[] = [2000, 4000, 5000, 3400, 7000, 8000, 6900, 7050, 9000, 8500, 7300, 9300];
+
   constructor() {
   }
 
   ngOnInit(): void {
     const hideNavHp = document.querySelector('#header');
-    const hideFooterHp = document.querySelector('.footer_container');
+    const hideFooterHp = document.querySelector('.footer__container');
     // @ts-ignore
     // tslint:disable-next-line:no-unused-expression
     hideNavHp.style.display = 'none';
     // @ts-ignore
     // tslint:disable-next-line:no-unused-expression
     hideFooterHp.style.display = 'none';
+
+    this.userVisitChart = new Chart('userVisitChart', {
+      type: 'bar',
+      data: {
+        labels: this.defLabelArr,
+        datasets: [{
+          label: 'Users bidding in 2022 year',
+          data: this.defDataArr,
+          backgroundColor: '#11b683'
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true
+          },
+          y: {
+            beginAtZero: true,
+            display: true,
+            max: 10000
+          }
+        }
+      }
+    });
   }
 
 }
--- auntion-product/src/app/product/statistic/statistic.component.ts
+++ auntion-product/src/app/product/statistic/statistic.component.ts
@@ -106,16 +106,25 @@ export class StatisticComponent implements OnInit {
   defaultDataArr: any[] = [];
   count: number;
 
+  message: string = null;
   private locale;
 
   ngOnInit(): void {
+    const hideNavHp = document.querySelector('#header');
+    const hideFooterHp = document.querySelector('.footer__container');
+    // @ts-ignore
+    // tslint:disable-next-line:no-unused-expression
+    hideNavHp.style.display = 'none';
+    // @ts-ignore
+    // tslint:disable-next-line:no-unused-expression
+    hideFooterHp.style.display = 'none';
     // this.statsBegin = this.datePipe.transform('', 'yyyy-MM-dd');
     // this.statsEnd = this.datePipe.transform('', 'yyyy-MM-dd');
     this.statsGroup = this.formBuilder.group({
       keyword: new FormControl(''),
       statsBegin: new FormControl(this.datePipe.transform('', 'yyyy-MM-dd'), Validators.required),
       statsEnd: new FormControl(this.datePipe.transform('', 'yyyy-MM-dd'), Validators.required)
-    });
+    }, checkDateDif(this.statsBegin, this.statsEnd));
     const localDate = new Date(Date.now());
     console.log(localDate);
     const month = localDate.getMonth() + 1;
@@ -183,62 +192,72 @@ export class StatisticComponent implements OnInit {
     // console.log(this.datePipe.transform(this.statsBeginDate, 'yyyy-MM-dd'));
     // console.log(this.datePipe.transform(this.statsEndDate, 'yyyy-MM-dd'));
     // Check if report too much times and the products get many duplicated values
-    this.productService.statsProductFromDateToDate(this.statsBegin,
-      this.statsEnd, 3).subscribe(items => {
-        for (const i in items) {
-          console.log('item: ' + items[i]);
-          if (this.products.find((test) => test.idProduct === items[i].idProduct) === undefined) {
-            this.products.push(items[i]);
-          }
+    if (this.datePipe.transform(this.statsBegin, 'yyyy-MM-dd') <= this.datePipe.transform(this.statsEnd, 'yyyy-MM-dd')) {
+      this.productService.statsProductFromDateToDate(this.statsBegin,
+        this.statsEnd, 3).subscribe(items => {
+          for (const i in items) {
+            console.log('item: ' + items[i]);
+            if (this.products.find((test) => test.idProduct === items[i].idProduct) === undefined) {
+              this.products.push(items[i]);
+            }
 
-        }
-        console.log('products:' + this.products);
-        getDataProduct(this.labelArr, this.dataArr, this.count, this.products);
-        console.log('Array products name in x Axis: ' + this.labelArr);
-        console.log('Array products finalPrice in y Axis: ' + this.dataArr);
-        if (this.myChart != null) {
-          this.myChart.destroy();
-        }
-        this.myChart = new Chart('myChart', {
-          type: 'bar',
-          data: {
-            labels: this.labelArr,
-            datasets: [{
-              label: 'Revenues',
-              data: this.dataArr,
-              backgroundColor: '#11b683'
-            }]
-          },
-          options: {
-            responsive: true,
-            scales: {
-              x: {
-                display: true
-              },
-              y: {
-                beginAtZero: true,
-                display: true
-              }
-            }, plugins: {
-              title: {
-                display: true,
-                text: 'Statistic Bar Chart From ' + this.statsBegin.toString() + ' To ' + this.statsEnd.toString(),
-                padding: {
-                  top: 10,
-                  bottom: 30
+          }
+          console.log('products:' + this.products);
+          getDataProduct(this.labelArr, this.dataArr, this.count, this.products);
+          console.log('Array products name in x Axis: ' + this.labelArr);
+          console.log('Array products finalPrice in y Axis: ' + this.dataArr);
+          if (this.myChart != null) {
+            this.myChart.destroy();
+          }
+          this.myChart = new Chart('myChart', {
+            type: 'bar',
+            data: {
+              labels: this.labelArr,
+              datasets: [{
+                label: 'Revenues',
+                data: this.dataArr,
+                backgroundColor: '#11b683'
+              }]
+            },
+            options: {
+              responsive: true,
+              scales: {
+                x: {
+                  display: true
+                },
+                y: {
+                  beginAtZero: true,
+                  display: true
+                }
+              }, plugins: {
+                title: {
+                  display: true,
+                  text: 'Statistic Bar Chart From ' + this.statsBegin.toString() + ' To ' + this.statsEnd.toString(),
+                  padding: {
+                    top: 10,
+                    bottom: 30
+                  }
                 }
               }
             }
-          }
-        });
-      }, () => {
-        console.log(error);
-        this.route.navigateByUrl('/admin/error-500');
-      }
-    );
+          });
+        }, () => {
+          console.log(error);
+          this.route.navigateByUrl('/admin/error-500');
+        }
+      );
+    } else {
+      this.message = 'Start date is later than end date';
+    }
     console.log('Array products name in x Axis after: ' + this.labelArr); // empty
     console.log('Array products finalPrice in y Axis: ' + this.dataArr); // empty
   }
+
+  hide() {
+    document.getElementById('noti').hidden = true;
+    this.message = null;
+  }
+
 
   getRandomColor() {
     const letters = '0123456789ABCDEF'.split('');
@@ -261,8 +280,10 @@ function getDataProduct(labelArr: any[], dataArr: any[], length: number, source:
   console.log(length);
 }
 
-function checkDateDif() {
-
+function checkDateDif(startDay: any, endDay: any): { date_not_match: boolean } {
+  if (this.datePipe.transform(this.startDay, 'yyyy-MM-dd') <= this.datePipe.transform(this.endDay, 'yyyy-MM-dd')) {
+    return {date_not_match: true};
+  }
 }
 
 
