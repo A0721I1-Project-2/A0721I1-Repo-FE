b3d47ab28abbcc7573851678c75c51c29f87ed99
phanthanhtoan <toan27122404@gmail.com>
2022-07-24 00:30:58 +0700

    edit

--- auntion-product/src/app/product/list-product/list-product.component.ts
+++ auntion-product/src/app/product/list-product/list-product.component.ts
@@ -50,15 +50,15 @@ export class ListProductComponent implements OnInit {
       sellerName: [''],
       priceRange: [''],
       biddingStatus: ['']
-    });
+    })
   }
 
   getAll() {
     this.productService.getAllProduct(0).subscribe(productList => {
       if (productList !== null ) {
-        this.productList = productList.['content'];
-        this.totalPages = productList.['totalPages'];
-        this.setPage(this.totalPages);
+        this.productList = productList['content'];
+        this.totalPages = productList['totalPages'];
+        this.setPage(this.totalPages)
         console.log(productList);
       } else {
         this.notFoundStatus = true;
@@ -69,57 +69,57 @@ export class ListProductComponent implements OnInit {
     });
     this.productService.getAllBiddingStatus().subscribe(biddingStatusList => {
       this.biddingStatusList = biddingStatusList;
-    });
-  }
+    })
+  };
 
   search() {
     // this.searchProduct.value.nameProduct,
     //   this.searchProduct.value.typeProduct, this.searchProduct.value.sellerName,maxPrice,minPrice,
     //   this.searchProduct.value.biddingStatus
 
-    if (this.searchProduct.value.nameProduct === '') {
+    if(this.searchProduct.value.nameProduct === "") {
       this.searchProduct.value.nameProduct = 'null';
-    }
+    };
 
-    if (this.searchProduct.value.typeProduct === '') {
-      this.searchProduct.value.typeProduct = 'null';
-    }
+    if (this.searchProduct.value.typeProduct === "") {
+      this.searchProduct.value.typeProduct = 'null'
+    };
 
-    if (this.searchProduct.value.sellerName === '') {
-      this.searchProduct.value.sellerName = 'null';
-    }
+    if (this.searchProduct.value.sellerName === "") {
+      this.searchProduct.value.sellerName = 'null'
+    };
 
-    if (this.searchProduct.value.biddingStatus === '') {
-      this.searchProduct.value.biddingStatus = 'null';
-    }
+    if (this.searchProduct.value.biddingStatus === "") {
+      this.searchProduct.value.biddingStatus = 'null'
+    };
 
-    if (this.searchProduct.value.priceRange === '') {
-      this.searchProduct.value.priceRange = 'null';
-    }
+    if (this.searchProduct.value.priceRange === "") {
+      this.searchProduct.value.priceRange = 'null'
+    };
 
     let minPrice: string;
     let maxPrice: string;
 
     switch (this.searchProduct.value.priceRange) {
-      case '0 - 100 USD':
-        minPrice = '0';
-        maxPrice = '100';
+      case "0 - 100 USD":
+        minPrice = "0";
+        maxPrice = "100";
         break;
-      case '100 - 500 USD':
-        minPrice = '100';
-        maxPrice = '500';
+      case "100 - 500 USD":
+        minPrice = "100";
+        maxPrice = "500";
         break;
-      case '500 - 5,000 USD':
-        minPrice = '500';
-        maxPrice = '5000';
+      case "500 - 5,000 USD":
+        minPrice = "500";
+        maxPrice = "5000";
         break;
-      case '5,000 - 50,000 USD':
-        minPrice = '5000';
-        maxPrice = '50000';
+      case "5,000 - 50,000 USD":
+        minPrice = "5000";
+        maxPrice = "50000";
         break;
-      case 'Above 50,000 USD':
-        minPrice = '50000';
-        maxPrice = '1000000';
+      case "Above 50,000 USD":
+        minPrice = "50000";
+        maxPrice = "1000000";
         break;
       default:
         minPrice = 'null';
@@ -127,21 +127,21 @@ export class ListProductComponent implements OnInit {
     }
 
     this.productService.searchByNameTypeSellerPriceStatus(this.searchProduct.value.nameProduct,
-      this.searchProduct.value.typeProduct, this.searchProduct.value.sellerName, maxPrice, minPrice,
-      this.searchProduct.value.biddingStatus, 0).subscribe(productList => {
+      this.searchProduct.value.typeProduct, this.searchProduct.value.sellerName,maxPrice,minPrice,
+      this.searchProduct.value.biddingStatus,0).subscribe(productList => {
       if (productList == null) {
         this.notFoundStatus = true;
       } else {
         this.notFoundStatus = false;
-        this.productList = productList.content;
-        this.totalPages = productList.totalPages;
+        this.productList = productList['content'];
+        this.totalPages = productList['totalPages'];
         this.setPage(this.totalPages);
       }
-    });
-  }
+    })
+  };
 
   onChangeDelete($event: any) {
-    this.idSelectedProduct = $event.target.value;
+    this.idSelectedProduct= $event.target.value;
     this.productSelected = $event.target.checked;
     console.log(this.productSelected);
     if (this.productSelected) {
@@ -161,7 +161,7 @@ export class ListProductComponent implements OnInit {
     }
   }
 
-  // Pagination
+  //Pagination
   nextPage() {
     if (this.pageNumber === this.totalPagesArray.length) {
       alert('Không thể chuyển qua trang sau!');
@@ -169,7 +169,7 @@ export class ListProductComponent implements OnInit {
       this.pageNumber += 1;
       this.showPage(this.pageNumber);
     }
-  }
+  };
 
   previousPage() {
     if (this.pageNumber <= 0) {
@@ -178,24 +178,24 @@ export class ListProductComponent implements OnInit {
       this.pageNumber -= 1;
       this.showPage(this.pageNumber);
     }
-  }
+  };
 
   private setPage(totalPages: any) {
     this.totalPagesArray = new Array(totalPages);
-  }
+  };
 
   changePageNumber(i: number) {
     this.pageNumber = i;
     this.showPage(this.pageNumber);
-  }
+  };
 
   showPage(page: number) {
     this.productService.getAllProduct(page).subscribe(productList => {
       if (productList !== null ) {
-        this.productList = productList.content;
-        this.totalPages = productList.totalPages;
+        this.productList = productList['content'];
+        this.totalPages = productList['totalPages'];
         this.setPage(this.totalPages);
       }
-    });
+    })
   }
 }
